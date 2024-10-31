6fa287480ba2965372eaa11c9ecf2ebe72b8d02a
Carlos Eduardo Taborda Lottermann <cadutaborda@gmail.com>
2024-10-31 11:44:40 -0300

    Fix filepath

--- src/system/src/sensor.cpp
+++ src/system/src/sensor.cpp
@@ -119,14 +119,14 @@ int main(int argc, char *argv[])
 
   // initialize 2 publisher nodes
   // std::string file_path_1 = "/home/windsurff/ros0_ws/src/system/src/number.txt";
-  std::string file_path_1 = "/home/ws/src/system/src/number.txt";
+  std::string file_path_1 = "/home/ws/src/system/src/temperature_data.txt";
   bool status_1 = true;
   auto options_1 = rclcpp::NodeOptions().arguments({"--ros-args", "-r", "__node:=sensor_1"});
   int32_t sensor_id_1 = 1;
   auto node_1 = std::make_shared<Sensor>(sensor_id_1, status_1, file_path_1, options_1, "trm");
 
   // std::string file_path_2 = "/home/windsurff/ros0_ws/src/system/src/numberstring.txt";
-  std::string file_path_2 = "/home/ws/src/system/src/numberstring.txt";
+  std::string file_path_2 = "/home/ws/src/system/src/heart_rate.txt";
   bool status_2 = true;
   auto options_2 = rclcpp::NodeOptions().arguments({"--ros-args", "-r", "__node:=sensor_2"});
   int32_t sensor_id_2 = 2;
